0ce9f8a2086cd08e1769f36c4157b031ddbbfbfe
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-04 00:08:38 +0600

    initial project set-up & 28.3 Component & JSX 28.4 Fragment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FirstComponent from './first_component'
+
+function App() {
+
+  return (
+    <>
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/first_component.jsx
@@ -0,0 +1,7 @@
+// PascalCasing
+
+const FirstComponent = () => {
+    return <h1>Farhan</h1>
+}
+
+export default FirstComponent;
