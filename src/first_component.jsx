6170c21ebb0039a9dd3e536755b6e0b2275ac910
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-05 10:55:21 +0600

    28.4 Conditional Rendering

--- src/first_component.jsx
+++ src/first_component.jsx
@@ -1,7 +1,43 @@
 // PascalCasing
 
 const FirstComponent = () => {
-    return <h1>Farhan</h1>
+    // const  name = "Nadim";
+
+    // Type 1:
+    // if (name) {
+    //     return (
+    //         <>
+    //             <h2>Hello {name}</h2>
+    //         </>
+    //     );
+    // }
+
+    // return (
+    //     <>
+    //         <h2>Hello World</h2>
+    //     </>
+    // );  
+
+
+
+    // Type 2:
+    // return (
+    //     <>
+    //         <h2>Hello { name ? name: "World" } </h2>
+    //     </>
+    // );  
+
+
+
+    // Type 3:
+    return (
+        <>
+            <h2>Hello { name && "World" } </h2>
+            <h2>Hello { name || "World" } </h2>
+        </>
+    );  
+
+
 }
 
 export default FirstComponent;
