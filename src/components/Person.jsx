cb5cbe3d6e5175ddae26c0b8d0ec4a42a3092cf9
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-08 19:25:08 +0600

    Spread Operator --> Refactor Person component to use a single state object for person details and update name handling using Spread Operator

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,20 +2,38 @@ import { useState } from "react";
 
 const Person = () => {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  // const [firstName, setFirstName] = useState("");   // This is not a good practice
+  // const [lastName, setLastName] = useState("");     //
+
+  const [person, setPerson] = useState({
+    firstName: "Farhan",
+    lastName: "Nadim",
+    address: "Dhaka",
+    phone: "123",
+    email: "n@email.com",
+  });
 
   const HandleChangeName = () => {
-    setFirstName("Farhan");
-    setLastName("Nadim");
+    // person.firstName = "Mr";
+    // person.lastName = "Nadim";     // It's wont work because React-এ state immutable
+    // setPerson(person);
+    // Thats why we need ---->  Spread Operator
 
-    // -> 
-    console.log(firstName, lastName); // We can't find the updated value here because the state is asynchronous
+    const newPerson = {
+      ...person,
+      firstName: "Mr.",
+      phone: "123456789"
+    }
+
+    setPerson(newPerson)
   }
 
   return (
     <div className="p-10">
-      <h1 className="mb-10 text-2xl font-semibold">Full Name: {firstName} {lastName}</h1>
+      <h1 className="text-2xl font-semibold">Full Name: {person.firstName} {person.lastName}</h1>
+      <h1> {person.address} </h1>
+      <h1> {person.phone} </h1>
+      <h1 className="mb-10"> {person.email} </h1>
       <button
         onClick={HandleChangeName}
         className="p-5 bg-blue-400 rounded-2xl"
