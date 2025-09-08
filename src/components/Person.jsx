0e2a52253df520392c8a79cd78eb889b286a887a
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-08 20:12:27 +0600

    Updating Single Object

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 
 const Person = () => {
 
-  // const [firstName, setFirstName] = useState("");   // This is not a good practice
-  // const [lastName, setLastName] = useState("");     //
-
   const [person, setPerson] = useState({
     firstName: "Farhan",
     lastName: "Nadim",
@@ -14,15 +11,13 @@ const Person = () => {
   });
 
   const HandleChangeName = () => {
-    // person.firstName = "Mr";
-    // person.lastName = "Nadim";     // It's wont work because React-এ state immutable
-    // setPerson(person);
-    // Thats why we need ---->  Spread Operator
 
+    // Update Person  & Add new Field
     const newPerson = {
-      ...person,
-      firstName: "Mr.",
-      phone: "123456789"
+        ...person,
+        firstName: "Mr.",
+        phone: "123456789",
+        department: "CSE",    // Added new field to person, 
     }
 
     setPerson(newPerson)
@@ -33,7 +28,8 @@ const Person = () => {
       <h1 className="text-2xl font-semibold">Full Name: {person.firstName} {person.lastName}</h1>
       <h1> {person.address} </h1>
       <h1> {person.phone} </h1>
-      <h1 className="mb-10"> {person.email} </h1>
+      <h1> {person.email} </h1>
+      <h1 className="mb-10"> {person.department} </h1>
       <button
         onClick={HandleChangeName}
         className="p-5 bg-blue-400 rounded-2xl"
