c26314438138b4c6929fac0777cf00f3b5bb12e7
Farhan Nadim <farhannadim2023@gmail.com>
2025-09-13 15:07:42 +0600

    30.5 Form Validation

--- src/components/Forms/HookForm.jsx
+++ src/components/Forms/HookForm.jsx
@@ -2,7 +2,11 @@ import { useForm } from "react-hook-form";
 
 const HookForm = () => {
 
-  const {register, handleSubmit} = useForm();
+  const {
+    register, 
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -18,12 +22,18 @@ const HookForm = () => {
             Name
           </label>
           <input
-            {...register("personName")}
-            id="name" 
-            type="text" 
-            placeholder="Enter Your Name" 
+            {...register("personName", { required: true, minLength: 5 })}
+            id="name"
+            type="text"
+            placeholder="Enter Your Name"
             className="p-3 border rounded-xl w-full my-2 focus:outline-none focus:ring-1 focus:border-none focus:ring-blue-600"
           />
+          {errors.personName?.type === "required" && <span className=" text-red-500">● This Field is Required</span>}
+          {/*               ^  optional chaining operator (?.)          */}
+          {errors.personName?.type === "minLength" && <span className=" text-red-500">● Name must be 5 characters long</span>}
+
+
+          {/* {console.log(errors.personName.type)} */}
         </div>
 
         <div>
@@ -33,12 +43,14 @@ const HookForm = () => {
             Age
           </label>
           <input
-            {...register("personAge")}
+            {...register("personAge", { required:true, min: 18 })}
             id="age" 
             type="number" 
             placeholder="Enter Your age" 
             className="p-3 border rounded-xl w-full my-2 focus:outline-none focus:ring-1 focus:border-none focus:ring-blue-600"
           />
+          {errors.personAge?.type === "required" && <span className=" text-red-500">● This Field is Required</span>}
+          {errors.personAge?.type === "min" && <span className=" text-red-500">● You must be at least 18 years old</span>}
         </div>
 
         <button
